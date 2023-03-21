e37bd2a77b53cb983aca1b75585ddeb3429295d2
rajarajan-uitoux <82135061+rajarajan-uitoux@users.noreply.github.com>
2023-03-21 22:27:25 +0530

    Phantom cart

--- src/pages/AuthenticatedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function AuthenticatedRoute() {
-  const token = localStorage.getItem("token");
-
-  return <>{token ? <Outlet /> : <Navigate to="/sign-in" />}</>;
-}
---
+++ src/store/cartSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+  billing: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addCart: (state, action) => {
+      let id = action.payload.product.id;
+      const existItem = state.cart.find((item) => item.id === id);
+      if (existItem) {
+            state.cart = state.cart.filter((item) => item.id !== id);
+            state.cart.push({ ...existItem, quantity: existItem.quantity + 1 });
+        } else {
+          state.cart.push({ ...action.payload.product, quantity: 1 });
+      }
+    },
+    updateCart: (state, action) => {
+        let id = action.payload.product.id;
+        const existItem = state.cart.find((item) => item.id === id);
+        if ((existItem.quantity + action.payload.quantity) > 0 ) {
+              state.cart = state.cart.filter((item) => item.id !== id);
+              state.cart.push({ ...existItem, quantity: existItem.quantity + action.payload.quantity });
+          } else {
+            state.cart = state.cart.filter((item) => item.id !== id);
+        }
+    },
+    removeCart: (state, action) => {
+        let id = action.payload.id;
+        state.cart = state.cart.filter((item) => item.id !== id);
+    },
+    addBilling: (state, action) => {
+      state.billing = [...action.payload.product];
+    },
+  },
+});
+
+export const { addCart, updateCart, removeCart, addBilling } = cartSlice.actions;
+
+export default cartSlice.reducer;
